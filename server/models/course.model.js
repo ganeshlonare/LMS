bc50fa8319e1dff4a1f2672d13b43233734154b5
ganeshlonare <ganeshlonare311@gmail.com>
2024-06-15 22:09:40 +0530

    Create course model and routes.

---
+++ server/models/course.model.js
@@ -0,0 +1,61 @@
+import {model,Schema} from 'mongoose'
+
+const courseSchema=new Schema({
+    title:{
+        type:String,
+        required:[true,'title is required '],
+        minLength:[8,'Title must be atleast 8 characters'],
+        maxLength:[60,'Title should be less than 60 characters'],
+        trim:true
+    },
+    description:{
+        type:String,
+        required:[true,'Description is required '],
+        minLength:[8,'Title must be atleast 8 characters'],
+        maxLength:[200,'Title should be less than 60 characters']
+    },
+    category:{
+        type:String,
+        required:[true,"Category is required"]
+        
+    },
+    thumbnail:{
+        public_id:{
+            type:String,
+            
+        },
+        secure_url:{
+            type:String,
+          
+        }
+
+    },
+    lectures:[
+        {
+            title:String,
+            description:String,
+            lecture:{
+                public_id:{
+                    type:String
+                },
+                secure_url:{
+                    type:String
+                }
+            }
+        }
+    ],
+    NumberOfLectures:{
+        type:Number,
+        default:0
+    },
+    createdBy:{
+        type:String,
+        required:true
+    }
+},{
+    timestamps:true
+})
+
+const Course=model('Course',courseSchema)
+
+export default Course
--- server/routes/course.route.js
+++ server/routes/course.route.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { getAllCourses } from "../controllers/course.controller.js";
+import { getAllCourses, getLecturesById } from "../controllers/course.controller.js";
+import { isUserLoggedIn } from "../middlewares/authMiddleware.js";
 
 const router=Router()
 
 router.route('/')
     .get(getAllCourses)
+
+router.route('/:id')
+    .get(isUserLoggedIn,getLecturesById)
 
 export default router
